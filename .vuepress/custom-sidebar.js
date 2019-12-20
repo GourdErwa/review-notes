5c86589509bd350b4ba4ebe33e79fc5f86fa7640
GourdErwa <gourderwa@163.com>
2019-12-20 18:02:18 +0800

    update docs

--- .vuepress/custom-sidebar.js
+++ .vuepress/custom-sidebar.js
@@ -1,7 +1,7 @@
 // 侧导航定义
 module.exports =
     {
-        '/language/java-concurrency/': require("../language/java-concurrency/java-concurrency.js"),
-        '/framework/flink-basis/': require("../framework/flink-basis/flink-basis")
+        '/language/java-concurrency/': require("../language/java-concurrency/_sidebar.js"),
+        '/framework/flink-basis/': require("../framework/flink-basis/_sidebar")
     }
 ;
