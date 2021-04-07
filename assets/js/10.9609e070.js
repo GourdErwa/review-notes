ff94d02d3c9979feb2f46b7cf35b495479ae54fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-07 07:08:41 +0000

    Deploy GourdErwa/review-notes to github.com/GourdErwa/review-notes.git:gh-pages

---
+++ assets/js/10.9609e070.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{384:function(t,e,s){"use strict";s.r(e);var n=s(10),o=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("blockquote",[e("p",[this._v("for test page.")])])])}),[],!1,null,null,null);e.default=o.exports}}]);
